8b210e004ef75bc5f45f5df93daf944b42f93032
xxliasprimeaux <alexiuspf3@gmail.com>
2022-09-22 09:54:12 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Home</title>
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="vendors/linericon/style.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
+    <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+    <link rel="stylesheet" href="vendors/animate-css/animate.css">
+    <link rel="stylesheet" href="vendors/popup/magnific-popup.css">
+
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/"><strong>PRIM</strong></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" datatarget="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" arialabel="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item ">
+                <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" href="/portfolio">Portfolio</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container text-center">
+        <div class="main_title">
+            <h1>WELCOME</h1>
+            <p>TO MY PAGE</p>
+        </div>
+
+        <section class="profile_area">
+            <div class="container text-left">
+                <div class="profile_inner p_120">
+                 <div class="row">
+                     <div class="col-lg-5">
+                         <img class="img-fluid" src="img/pfp.jpg" alt="">
+                     </div>
+                     <div class="col-lg-7">
+                         <div class="personal_text">
+                             <h6>Hello Everybody, i am</h6>
+                             <h3>Alexius Prima Fabianto</h3>
+                             <h4>Graphic Designer</h4>
+                             <p>Happy, free, confused and lonely at the same time.
+                                It's miserable and magical.</p>
+                             <ul class="list basic_info">
+                                 <li><a href="#"><i class="lnr lnr-calendar-full"></i> February 20th, 1999</a></li>
+                                 <li><a href="#"><i class="lnr lnr-phone-handset"></i> 0882 1809 4203</a></li>
+                                 <li><a href="#"><i class="lnr lnr-envelope"></i> alexiuspf3@gmail.com</a></li>
+                                 <li><a href="#"><i class="lnr lnr-home"></i> Bandung, Indonesia</a></li>
+                             </ul>
+                             <ul class="list personal_social">
+                                 <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                 <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                 <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                             </ul>
+                         </div>
+                     </div>
+                 </div>
+                </div>
+         </div>
+     </section>
+
+     <section class="welcome_area p_120">
+         <div class="container text-left">
+             <div class="row welcome_inner">
+                 <div class="col-lg-6">
+                     <div class="welcome_text">
+                         <h4>About Myself</h4>
+                         <p>Peoples find expert designers to complete their work and I am an expert designer to complete all types of work. I have the experience to design high-quality business cards, logos, and social media posts.
+                            I have experience more than 5 years in this field and provide work on many freelancing websites.
+
+                            I have also a portfolio website where you can check my expertise and give me chance to complete your work.
+
+                            I have completed 100+ orders with 5.0 customer satisfaction that you can see on freelancing platforms.
+
+                            If you have any type of design related work , I am here for you to get quality work at a minimum time and affordable price.</p>
+                         <div class="row">
+                             <div class="col-md-4">
+                                 <div class="wel_item">
+                                     <i class="lnr lnr-database"></i>
+                                     <h4>$0</h4>
+                                     <p>Total Donation</p>
+                                 </div>
+                             </div>
+                             <div class="col-md-4">
+                                 <div class="wel_item">
+                                     <i class="lnr lnr-book"></i>
+                                     <h4>500</h4>
+                                     <p>Total Projects</p>
+                                 </div>
+                             </div>
+                             <div class="col-md-4">
+                                 <div class="wel_item">
+                                     <i class="lnr lnr-users"></i>
+                                     <h4>0</h4>
+                                     <p>Total Volunteers</p>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="col-lg-6">
+                     <div class="tools_expert">
+                         <div class="skill_main">
+                             <div class="skill_item">
+                                 <h4>Affinity Designer <span class="counter">90</span>%</h4>
+                                 <div class="progress_br">
+                                     <div class="progress">
+                                         <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                     </div>
+                                 </div>
+                             </div>
+                             <div class="skill_item">
+                                 <h4>CorelDraw <span class="counter">85%</span>%</h4>
+                                 <div class="progress_br">
+                                     <div class="progress">
+                                         <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                                     </div>
+                                 </div>
+                             </div>
+                             <div class="skill_item">
+                                 <h4>Illustrator <span class="counter">70</span>%</h4>
+                                 <div class="progress_br">
+                                     <div class="progress">
+                                         <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                                     </div>
+                                 </div>
+                             </div>
+                             <div class="skill_item">
+                                 <h4>Procreate <span class="counter">90</span>%</h4>
+                                 <div class="progress_br">
+                                     <div class="progress">
+                                         <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                     </div>
+                                 </div>
+                             </div>
+                             <div class="skill_item">
+                                 <h4>Photoshop <span class="counter">50</span>%</h4>
+                                 <div class="progress_br">
+                                     <div class="progress">
+                                         <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </section>
+
+        <small>Copyright © <?php echo date("Y"); ?> LIKMI </small>
+
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/popper.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/stellar.js"></script>
+        <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+        <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+        <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+        <script src="vendors/isotope/isotope.pkgd.min.js"></script>
+        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="vendors/popup/jquery.magnific-popup.min.js"></script>
+        <script src="js/jquery.ajaxchimp.min.js"></script>
+        <script src="vendors/counter-up/jquery.waypoints.min.js"></script>
+        <script src="vendors/counter-up/jquery.counterup.min.js"></script>
+        <script src="js/mail-script.js"></script>
+        <script src="js/theme.js"></script>
+        <script src="/js/app.js"></script> <br><br>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Home</title>
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="vendors/linericon/style.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
+    <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+    <link rel="stylesheet" href="vendors/animate-css/animate.css">
+    <link rel="stylesheet" href="vendors/popup/magnific-popup.css">
+
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/"><strong>PRIM</strong></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" datatarget="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" arialabel="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item ">
+                <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" href="/portfolio">Portfolio</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container text-center">
+        <div class="main_title">
+            <h1>PORTFOLIO</h1>
+            <p>DESIGNS MADE BY ME</p>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+              <img
+                src="/img/1.png"
+                class="w-100 shadow-1-strong rounded mb-4"
+                alt="Boat on Calm Water"
+              />
+
+              <img
+                src="/img/2.jpg"
+                class="w-100 shadow-1-strong rounded mb-4"
+                alt="Wintry Mountain Landscape"
+              />
+            </div>
+
+            <div class="col-lg-4 mb-4 mb-lg-0">
+              <img
+                src="/img/3.jpg"
+                class="w-100 shadow-1-strong rounded mb-4"
+                alt="Mountains in the Clouds"
+              />
+
+              <img
+                src="/img/4.jpg"
+                class="w-100 shadow-1-strong rounded mb-4"
+                alt="Boat on Calm Water"
+              />
+            </div>
+
+            <div class="col-lg-4 mb-4 mb-lg-0">
+              <img
+                src="/img/5.jpg"
+                class="w-100 shadow-1-strong rounded mb-4"
+                alt="Waves at Sea"
+              />
+
+              <img
+                src="/img/6.jpg"
+                class="w-100 shadow-1-strong rounded mb-4"
+                alt="Yosemite National Park"
+              />
+            </div>
+
+            <div class="col-lg-4 mb-4 mb-lg-0">
+              <img
+                src="/img/7.png"
+                class="w-100 shadow-1-strong rounded mb-4"
+                alt="Waves at Sea"
+              />
+
+              <img
+                src="/img/8.jpg"
+                class="w-100 shadow-1-strong rounded mb-4"
+                alt="Yosemite National Park"
+              />
+            </div>
+          </div>
+
+        <small>Copyright © <?php echo date("Y"); ?> LIKMI </small>
+
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/popper.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/stellar.js"></script>
+        <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+        <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+        <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+        <script src="vendors/isotope/isotope.pkgd.min.js"></script>
+        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="vendors/popup/jquery.magnific-popup.min.js"></script>
+        <script src="js/jquery.ajaxchimp.min.js"></script>
+        <script src="vendors/counter-up/jquery.waypoints.min.js"></script>
+        <script src="vendors/counter-up/jquery.counterup.min.js"></script>
+        <script src="js/mail-script.js"></script>
+        <script src="js/theme.js"></script>
+        <script src="/js/app.js"></script> <br><br>
+    </div>
+</body>
+
+</html>
